92b8ee00381354ffee7b72f9992f975600d293cf
Piotr Janaszek <piotr.janaszek@gmail.com>
2024-09-17 11:57:45 +0200

    Printing arrows

---
+++ arrow.py
@@ -0,0 +1,15 @@
+ # Minimize the number of print() function invocations by inserting the \n sequence into the strings
+print("    *", "   * *", "  *   *"," *     *","***   ***","  *   *","  *   *","  *****", sep='\n')
+
+# Make the arrow twice as large (but keep the proportions)
+print(" "," "," "," ","*")
+print(" "," "," ","*"," ","*")
+print(" "," ","*"," "," "," ","*")
+print(" ","*"," "," "," "," "," ","*")
+print("*","*","*"," "," "," ","*","*","*")
+print(" "," ","*"," "," "," ","*")
+print(" "," ","*"," "," "," ","*")
+print(" "," ","*","*","*","*","*")
+
+# Duplicate the arrow, placing both arrows side by side
+print("    *    "*2, "   * *   "*2, "  *   *  "*2," *     * "*2,"***   ***"*2,"  *   *  "*2,"  *   *  "*2,"  *****  "*2, sep='\n')
---
+++ essentials.py
@@ -0,0 +1,3 @@
+# Expected output: Programming***Essentials***in...Python
+print('Programming', 'Essentials', 'in', sep='***', end='...')
+print('Python')
